d89a904e176aef7f1284d5a3262f5410e41440bf
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-10-08 12:40:48 -0500

    moved starter code to own directory

---
+++ classes/sports_league/starter_code/Team.h
@@ -0,0 +1,9 @@
+#ifndef _TEAM_H_
+#define _TEAM_H_
+
+// TODO:
+//   define class Team:
+//     data members - name, list of players, numWins, numLosses, numTies
+//     member functions - constructor, destructor, printRoster, printRecord
+
+#endif
