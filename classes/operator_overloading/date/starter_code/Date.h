b345c327473cef7f92de876b6534e6fb1b7e653a
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-10-15 13:20:48 -0500

    moved to starter_code directory

---
+++ classes/operator_overloading/date/starter_code/Date.h
@@ -0,0 +1,23 @@
+#ifndef _DATE_H_
+#define _DATE_H_
+
+#include <iostream>
+
+class Date {
+    private:
+        unsigned int month, day, year;
+
+    public:
+        Date(int m, int d, int y) : month(m), day(d), year(y) {}
+        ~Date() {}
+
+        void print() const {
+            std::cout << month << "/" << day << "/" << year;
+        }
+
+        //add comparison (==)
+
+        // add rank (<)
+};
+
+#endif
