5290c081d970abb511fe29f5634a38aaa8f32cce
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-10-15 13:24:30 -0500

    added testing of operators to implement

--- classes/operator_overloading/date/starter_code/main.cpp
+++ classes/operator_overloading/date/starter_code/main.cpp
@@ -3,6 +3,7 @@
 int main() {
     Date d1(1, 1, 2001);
     Date d2(10, 15, 2015);
+    Date d3(10, 15, 2015);
 
     std::cout << "Date 1: ";
     d1.print();
@@ -10,6 +11,22 @@ int main() {
     std::cout << "Date 2: ";
     d2.print();
     std::cout << std::endl;
+    std::cout << "Date 3: ";
+    d3.print();
+    std::cout << std::endl;
 
+    //implement ==, !=, <, and <= for Date class so this can work
+    std::cout << "Comparing Date 1 and Date 2:\n";
+    std::cout << "\t==: " << (d1 == d2) << std::endl;
+    std::cout << "\t!=: " << (d1 != d2) << std::endl;
+    std::cout << "\t<:  " << (d1 < d2) << std::endl;
+    std::cout << "\t<=: " << (d1 <= d2) << std::endl;
+
+    std::cout << "Comparing Date 2 and Date 3:\n";
+    std::cout << "\t==: " << (d2 == d3) << std::endl;
+    std::cout << "\t!=: " << (d2 != d3) << std::endl;
+    std::cout << "\t<:  " << (d2 < d3) << std::endl;
+    std::cout << "\t<=: " << (d2 <= d3) << std::endl;
+    
     return 0;
 }
