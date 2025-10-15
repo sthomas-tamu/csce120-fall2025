ec308bfb49ccf0a016e3c04258a23837e4789536
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-10-15 13:32:31 -0500

    implemented == and != in various ways

---
+++ classes/operator_overloading/dice/completed_code/Dice.h
@@ -0,0 +1,17 @@
+class Dice {
+  private:
+    unsigned int num_sides;
+    unsigned int current_value;
+
+  public:
+    Dice(unsigned int n) : num_sides(n), current_value(0) {}
+    ~Dice() {}
+
+    unsigned int getSides() const;
+    unsigned int getValue() const;
+
+    void roll();
+
+    bool operator==(const Dice& rhs) const;
+    bool operator!=(const Dice& rhs) const;
+};
