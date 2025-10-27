1911c6f5ff9bf23ab993a2b6cf64d5463d8b9ebd
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-10-26 21:35:00 -0500

    array examples

---
+++ arrays/c-array/array.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main() {
+    // declare array
+    int primes[5] = {2, 3, 5, 7, 11};
+
+    // print elements, iterating by const refererence
+    std::cout << "The primes array:";
+    for(const int& element : primes) {
+        std::cout << " " << element;
+    }
+    std::cout << std::endl;
+
+    // increment elements, iterate by reference
+    for(int& element : primes) {
+        element += 1;
+    }
+    std::cout << "The updated primes array:";
+    for(const int& element : primes) {
+        std::cout << " " << element;
+    }
+    std::cout << std::endl;
+
+    // try to revert, iterate by value
+    for(int element : primes) {
+        element -= 1;
+    }
+    std::cout << "The reverted primes array:";
+    for(const int& element : primes) {
+        std::cout << " " << element;
+    }
+    std::cout << std::endl;
+
+    // create a zero-initialized array of the same size
+    int primes_copy[5] = {};
+    std::cout << "The zero-initialized array:";
+    for(const int& element : primes_copy) {
+        std::cout << " " << element;
+    }
+    std::cout << std::endl;    
+
+    /*
+    // copy the array
+    primes_copy = primes;
+    std::cout << "The updated primes_copy array:\t";
+    for(const int& element : primes_copy) {
+        std::cout << " " << element;
+    }
+    std::cout << std::endl;   
+    */
+
+    return 0;
+}
