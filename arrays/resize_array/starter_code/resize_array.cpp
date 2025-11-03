271d7ef69c6a189d92c5f0d286289ba497fa8fff
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-11-03 13:28:37 -0600

    added some todo comments and updated print to show capacity

--- arrays/resize_array/starter_code/resize_array.cpp
+++ arrays/resize_array/starter_code/resize_array.cpp
@@ -16,26 +16,31 @@ int main() {
     unsigned int capacity = 3;
     unsigned int size = 0;
     int* ary = new int[capacity];
-    std::cout << "Initial array: ";
+    std::cout << "Initial array (capacity = " << capacity << " ): ";
     printAry(std::cout, ary, size);   
     std::cout << std::endl; 
  
     // Add elements, resizing when needed
     for (int i = 0; i < 10; i++) {
+        // TODO 3: Move the body of this loop into a push() function
         if (size == capacity) {
+            // TODO 1: Implement array resizing by doubling capacity
             std::cout << "Array full! Implement resizing\n";
-        } else {
-            // Add the new element
-            ary[size] = i*2;
-            size++;        
-            std::cout << "Added: " << ary[size-1] << std::endl;
-        }
+            break;
+        } 
+        // Add the new element
+        ary[size] = i*2;
+        size++;        
+        std::cout << "Added: " << ary[size-1] << std::endl;
     }
     
     // Display final array
-    std::cout << "Final array: ";
+    std::cout << "Final array (capacity = " << capacity << "): ";
     printAry(std::cout, ary, size);
     std::cout << std::endl;
+
+    // TODO 2: Shrink array to fit and display again
+    // TODO 3: Move the shrink array into a shrinkToFit() function
 
     // Release the memory 
     delete[] ary;
