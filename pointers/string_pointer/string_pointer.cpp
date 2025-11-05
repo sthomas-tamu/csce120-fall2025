91589a44493bb9e60cff25af0291305e245e76c4
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-11-05 13:29:07 -0600

    fixed typo

--- pointers/string_pointer/string_pointer.cpp
+++ pointers/string_pointer/string_pointer.cpp
@@ -5,16 +5,16 @@ int main() {
     // declare a string
     std::string str = "Howdy!";
     std::cout << "My string is: " << str << std::endl;
-    std::cout << "The first charater is " << str.at(0) << std::endl << std::endl;
+    std::cout << "The first character is " << str.at(0) << std::endl << std::endl;
 
     // declare a string pointer
     std::string* strPtr = &str;
     std::cout << "My string pointer is: " << strPtr << std::endl;
     std::cout << "The string it points to is: " << *strPtr << std::endl;
-    std::cout << "The first charater is " << (*strPtr).at(0) << std::endl;
+    std::cout << "The first character is " << (*strPtr).at(0) << std::endl;
 
     // another way to dereference the pointer
-    std::cout << "The first charater is " << strPtr->at(0) << std::endl;
+    std::cout << "The first character is " << strPtr->at(0) << std::endl;
 
     // modify the string through the pointer
     std::cout << std::endl << "Updating the first character through the string pointer:" << std::endl; 
