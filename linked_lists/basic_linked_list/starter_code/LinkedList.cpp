ad8da210c322023ae6e7b9b55c286f956e6ddd65
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-11-16 16:22:42 -0600

    starter code for basic linked list holding ints

---
+++ linked_lists/basic_linked_list/starter_code/LinkedList.cpp
@@ -0,0 +1,88 @@
+#include "LinkedList.h"
+
+LinkedList::LinkedList(const LinkedList& source) : head(nullptr), tail(nullptr) {
+    Node* cur = source.head;
+    while(cur != nullptr) {
+        insertBack(cur->data);
+        cur = cur->next;
+    }
+}
+
+
+LinkedList& LinkedList::operator=(const LinkedList& source) {
+    clear();
+    Node* cur = source.head;
+    while(cur != nullptr) {
+        insertBack(cur->data);
+        cur = cur->next;
+    }
+    return *this;
+}
+
+
+void LinkedList::clear() {
+    while (head != nullptr) {
+        Node* nodeToDelete = head;
+        head = head->next;
+        delete nodeToDelete;
+    }
+    // head is nullptr when loop exits
+
+    tail = nullptr;
+}
+
+
+void LinkedList::insertFront(const int& data) {
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode;
+    if (tail == nullptr) { // empty list
+        tail = newNode;
+    }
+}
+
+
+void LinkedList::insertBack(const int& data) {
+    Node* newNode = new Node(data); // newNode->next is set to nullptr in its constructor
+    if (tail == nullptr) { // empty list
+        head = tail = newNode;
+    } else {
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
+
+
+void LinkedList::insertAfter(const int& valueToInsert, const int& valueToLookFor) {
+    //TODO #2
+}
+
+
+void LinkedList::remove(const int& value) {
+    //TODO #3
+}
+
+
+void LinkedList::print(std::ostream& os) const {
+    Node* cur = head;
+    if (cur == nullptr) {
+        os << "Empty List!";
+    }
+    while (cur != nullptr) {
+        os << cur->data << " ";
+        cur = cur->next;
+    }
+    os << std::endl;
+}
+
+
+bool LinkedList::contains(const int& value) const {
+    //TODO #1
+    return false;
+}
+
+
+std::ostream& operator<<(std::ostream& os, const LinkedList& ll) {
+    ll.print(os);
+    return os;
+}
---
+++ linked_lists/basic_linked_list/starter_code/main.cpp
@@ -0,0 +1,53 @@
+#include "LinkedList.h"
+
+int main() {
+    LinkedList ll;
+    std::cout << "ll: constructor\n" << ll << std::endl;
+
+    ll.insertBack(8);
+    std::cout << "ll: insertBack(8)\n" << ll << std::endl;
+
+    ll.insertFront(3);
+    std::cout << "ll: insertFront(3)\n" << ll << std::endl;
+
+    LinkedList ll2(ll);
+    std::cout << "ll2: copy ll\n" << ll2 << std::endl;
+
+    ll.insertBack(7);
+    std::cout << "ll: insertBack(7)\n" << ll << std::endl;
+    std::cout << "ll2:\n" << ll2 << std::endl;
+
+    ll2 = ll;
+    std::cout << "ll2 = ll1\n" << ll2 << std::endl;
+
+    ll.clear();
+    std::cout << "ll: clear\n" << ll << std::endl;
+    std::cout << "ll2\n" << ll2 << std::endl;    
+    
+    /*
+    std::cout << "ll2 contains 8: " << std::boolalpha << ll2.contains(8) << std::endl;
+    std::cout << "ll2 contains 0: " << std::boolalpha << ll2.contains(0) << std::endl;
+
+    std::cout << "ll2:\n" << ll2 << std::endl;
+
+    ll2.insertAfter(-1, 0);
+    std::cout << "ll2 attempt insert -1 after 0:\n" << ll2 << std::endl;
+
+    ll2.insertAfter(-1, 8);
+    std::cout << "ll2 attempt insert -1 after 8:\n" << ll2 << std::endl;
+
+    ll2.remove(0);
+    std::cout << "remove 0 from ll2:\n" << ll2 << std::endl;
+
+    ll2.remove(8);
+    std::cout << "remove 8 from ll2:\n" << ll2 << std::endl;
+
+    ll2.remove(3);
+    std::cout << "remove 3 from ll2:\n" << ll2 << std::endl;
+
+    ll2.remove(7);
+    std::cout << "remove 7 from ll2:\n" << ll2 << std::endl;
+    */
+ 
+    return 0;
+}
