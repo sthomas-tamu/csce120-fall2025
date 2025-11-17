75d31e4184591f68ee3221b9efcb95904ca1f2bf
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-11-17 12:22:51 -0600

    completed contains, insertAfter, and remove functions

---
+++ linked_lists/basic_linked_list/completed_code/LinkedList.h
@@ -0,0 +1,38 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int data) : data(data), next(nullptr) {}
+};
+
+class LinkedList {
+    Node* head;
+    Node* tail;
+
+  public:
+    LinkedList() : head(nullptr), tail(nullptr) {}
+    LinkedList(const LinkedList&);
+    ~LinkedList() { clear(); }
+    LinkedList& operator=(const LinkedList&);
+
+    void clear();
+
+    void insertFront(const int&);
+    void insertBack(const int&);
+    void insertAfter(const int& valueToInsert, const int& valueToLookFor);
+
+    void remove(const int&);
+
+    void print(std::ostream& = std::cout) const;
+
+    bool contains(const int&) const;
+};
+
+std::ostream& operator<<(std::ostream&, const LinkedList&);
+
+#endif
