f9d18375eb971ee72b90536929b5d86a6f3ccf03
sthomas@cse.tamu.edu <sthomas@cse.tamu.edu>
2025-11-23 21:15:29 -0600

    starter code for Employee class hierarchy example

---
+++ inheritance/employee/starter_code/Employee.h
@@ -0,0 +1,22 @@
+#ifndef _EMPLOYEE_H_
+#define _EMPLOYEE_H_
+
+#include <string>
+#include <iostream>
+
+/* TODO #1: create an Employee class that
+             - stores firstName and lastName strings
+             - has a print function: void print(std::ostream& os) const
+*/
+
+/* TODO #2: create an HourlyEmployee derived off of Employee that 
+             - stores a hourlyWage double
+             - has a print function that calls the Employee version and also prints the hourly wage
+*/
+
+/* TODO #3: create a SalariedEmployee derived off of Employee that 
+             - stores an annualSalary double
+             - has a print function that calls the Employee version and also prints the annual salary
+*/
+
+#endif
---
+++ inheritance/employee/starter_code/driver.cpp
@@ -0,0 +1,17 @@
+#include "Employee.h"
+
+#include <iostream>
+
+int main (int argc, char * argv[]) {
+    HourlyEmployee emp1("James", "Williams", 18.50);
+    std::cout << "HourlyEmployee: ";
+    emp1.print(std::cout);
+    std::cout << std::endl;
+
+    SalariedEmployee emp2 {"Mary", "Smith", 134000};
+    std::cout << "SalariedEmployee: ";
+    emp2.print(std::cout);
+    std::cout << std::endl;
+
+    return 0;
+}
